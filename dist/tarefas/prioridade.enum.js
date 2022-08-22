040eba8d033f6bdfbc5973ebd0226eefc5a7a212
oscarraichert <oscarraichert@hotmail.com>
2022-08-22 06:05:43 -0300

    excluindo dist

--- dist/tarefas/prioridade.enum.js
+++
@@ -1,6 +0,0 @@
-export var Prioridade;
-(function (Prioridade) {
-    Prioridade["Baixa"] = "Baixa";
-    Prioridade["Media"] = "Media";
-    Prioridade["Alta"] = "Alta";
-})(Prioridade || (Prioridade = {}));
